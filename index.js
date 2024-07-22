7c8817b35c1f666010450fd9aaca8508fc73fc3a
pradeep <adhikaripradeep11@gmail.com>
2024-07-22 23:22:20 +0545

    initial

--- index.js
+++ index.js
@@ -57,6 +57,25 @@ app.get('/getsifaris', (req, res) =>{
 })
 
 
+
+//update Sifaris
+app.put("/update", async(req,res)=>{
+    console.log(req.body)
+    const { id,...rest} = req.body
+    console.log(rest)
+
+    const data =  await Sifaris.updateOne({_id : id},rest)
+    res.send({success: true, message:"data update successfully", data:data})
+
+})
+//delete sifaris
+app.delete("/delete/:id", async(req,res) =>{
+    const id = req.params.id
+    console.log(id)
+    const data = await Sifaris.deleteOne({_id : id})
+    res.send({success : true, message : "data deleted successfully", data : data})
+})
+
 app.listen(process.env.PORT || 5000, () => {
     console.log("App is running")
 })
